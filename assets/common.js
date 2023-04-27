f02c1a6412f003b592bf95bfda5f1b0dcd5c9cf8
aka-pharande <aka.pharande@gmail.com>
2023-04-27 19:52:58 -0400

    Initial Commit

---
+++ assets/common.js
@@ -0,0 +1,88 @@
+/*
+ * Student Name: Aakanksha Pharande
+ * Student ID: 041075173 
+ * Course: CST8117 - Cross-Platform Web Design
+ * Semester: 1
+ * Assignment: 4 
+ * Date Submitted: 11/12/2022
+ */
+
+
+/* 
+ * Navigation Bar (Header Section) 
+ * This section will provide a user the ability to move between different pages.
+ * This navigation bar should be same across all pages.
+ */
+/*
+ * Source: https://www.youtube.com/watch?v=At4B7A4GOPg
+ * Author: Web Dev Simplified
+ * Date Retrieved: 2022-11-20
+ */
+var nav = `
+<a href = "#main" class= "skip">Skip to main content</a>
+<nav class="navbar">
+    <div id="logo-container"> 
+    <a id="logo" href="index.html">
+        <img src="Images/logo.png" alt="Image by (HER Closet Boutique , n.d.)" title= "Her Closet logo" />
+    </a>
+    </div>
+    <a href="#" class="hamburger">
+    <span class="bar"></span>
+    <span class="bar"></span>
+    <span class="bar"></span>
+    </a>
+    <div class="navbar-links">
+    <div id="search_box">
+        <input type="text" placeholder="Search...">
+    </div>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about-us.html">About Us</a></li>
+        <li><a href="products.html">Products</a></li>
+        <li><a href="cart.html">Cart</a></li>
+        <li><a href="contact-us.html">Contact Us</a></li>
+    </ul>
+    </div>
+</nav>
+`;
+
+/* 
+ * Footer Section  
+ * This section will provide user with some additional 
+ * navigation and links to privacy and T&C.
+ * This footer section should be the same across all pages.
+ */
+var footer = `
+<div class="footer">
+    <h3> her closet </h3>
+    <ul class="links">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about-us.html">About Us</a></li>
+        <li><a href="products.html">Products</a></li>
+        <li><a href="cart.html">Cart</a></li>
+        <li><a href="contact-us.html">Contact Us</a></li>
+        <li><a href="terms.html">Terms & Conditions</a></li>
+        <li><a href="privacy.html">Privacy Policy</a></li>
+    </ul>
+    <div class="footer-copyright">
+        <h5>Copyright &copy; 2022 Her Closet. designed by Aakanksha</h5>
+    </div>
+</div>
+`;
+
+let headerElem = document.getElementById("header");
+if (headerElem) {
+    headerElem.innerHTML= nav;
+}
+
+let footerElem = document.getElementById("footer");
+if (footerElem) {
+    footerElem.innerHTML= footer;
+}
+
+const toggleButton = document.getElementsByClassName('hamburger')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
---
+++ assets/main.js
@@ -0,0 +1,188 @@
+/*
+ * Student Name: Aakanksha Pharande
+ * Student ID: 041075173 
+ * Course: CST8117 - Cross-Platform Web Design
+ * Semester: 1
+ * Assignment: 4 
+ * Date Submitted: 11/12/2022
+ */
+
+// Constructor function
+function Product(productName,image,price,featured,description) {
+    this.productName = productName;
+    this.image = image;
+    this.price = price;
+    this.featured = featured;
+    this.description = description;
+    this.displayProductListing = function() {
+        let product_html = '<div class="col">';
+        product_html += '<a href="product.html">';
+        product_html += '<img src="' + this.image + '" alt="Feature 1"  title="' + this.productName + '">';
+        product_html += '</a>';
+        product_html += '<h4>' + this.description + '</h4>';
+        product_html += '<h5>' + 'Price: $' + this.price + '</h5>';
+        product_html += '</div>';
+        return product_html;
+    }
+  };
+
+// Declaring an empty array named allProducts
+var allProducts = [];
+
+// Pushing 12 new Products to the array
+/*
+ * Source: https://vkornacheva.com/Photographer_in_Lavender_Fields
+ * Author: Kornacheva.com
+ * Date Retrieved: 2022-11-20
+ */
+allProducts.push(new Product("Offshoulder dress", "images/feature_1.jpeg", 100, true, "Beautiful white mini dress"));
+/*
+ * Source: https://vkornacheva.com/Photographer_in_Lavender_Fields
+ * Author: Kornacheva.com
+ * Date Retrieved: 2022-11-20
+ */
+allProducts.push(new Product("Gowns", "images/feature_2.jpeg", 200, true, "Charming Violet Gown"));
+/*
+ * Source: https://www.boredpanda.com/lavender-field-2/?utm_source=google&utm_medium=organic&utm_campaign=organic
+ * Author: Bored Panda
+ * Date Retrieved: 2022-11-20
+ */
+allProducts.push(new Product("Short dresses", "images/feature_3.jpeg", 300, true, "Short pink dress"));
+/*
+ * Source: https://1freewallpapers.com/cute-girl-is-standing-in-lavender-field-wearing-white-dress-in-purple-background-cute
+ * Author: 1FreeWallpapers
+ * Date Retrieved: 2022-11-20
+ */
+allProducts.push(new Product("Short dresses", "images/product_1.jpeg", 250, false, "Short pink dress"));
+/*
+ * Source: https://www.peakpx.com/en/hd-wallpaper-desktop-opccq
+ * Author: Peakpx
+ * Date Retrieved: 2022-11-20
+ */
+allProducts.push(new Product("Short dresses", "images/product_2.jpeg", 250, false, "Short pink dress"));
+/*
+ * Source: https://www.peakpx.com/en/hd-wallpaper-desktop-avlwr
+ * Author: Peakpx
+ * Date Retrieved: 2022-11-20
+ */
+allProducts.push(new Product("Short dresses", "images/product_3.jpeg", 250, false, "Short pink dress"));
+/*
+ * Source: https://www.mihaigonguta.com/photoshoot-in-lavender/
+ * Author: Mihai Gonguta
+ * Date Retrieved: 2022-11-20
+ */
+allProducts.push(new Product("Short dresses", "images/product_4.jpeg", 250, false, "Short pink dress"));
+/*
+ * Source: https://www.wallpaperflare.com/beautiful-woman-dress-and-lavender-field-beauty-blonde-dancing-wallpaper-azkvt
+ * Author: Wallpaper Flare
+ * Date Retrieved: 2022-11-20
+ */
+allProducts.push(new Product("Short dresses", "images/product_5.jpeg", 250, false, "Short pink dress"));
+/*
+ * Source: https://www.wallpaperflare.com/woman-walking-on-lavender-field-standing-woman-surrounded-by-lavender-flower-during-daytime-wallpaper-zpaec
+ * Author: Wallpaper Flare
+ * Date Retrieved: 2022-11-20
+ */
+allProducts.push(new Product("Short dresses", "images/product_6.jpeg", 250, false, "Short pink dress"));
+/*
+ * Source: https://best-wallpaper.net/Purple-skirt-girl-lavender-flowers-field-butterfly_wallpapers.html
+ * Author: Best Wallpaper
+ * Date Retrieved: 2022-11-20
+ */
+allProducts.push(new Product("Short dresses", "images/product_7.jpeg", 250, false, "Short pink dress"));
+/*
+ * Source: https://nature.desktopnexus.com/wallpaper/2325098/
+ * Author: Desktop Nexus
+ * Date Retrieved: 2022-11-20
+ */
+allProducts.push(new Product("Short dresses", "images/product_8.jpeg", 250, false, "Short pink dress"));
+/*
+ * Source: https://vkornacheva.com/Photographer_in_Lavender_Fields
+ * Author: Kornacheva.com
+ * Date Retrieved: 2022-11-20
+ */
+allProducts.push(new Product("Short dresses", "images/product_9.jpeg", 250, false, "Short pink dress"));
+
+// Declaring a function in the global context named isFeaturedProduct
+function isFeaturedProduct(prod) {
+    return prod.featured;
+}
+
+// Declaring a function named getFeaturedProduct which returns 
+// filtered products from all products, where featured = true
+function getFeaturedProducts() {
+    return allProducts.filter(isFeaturedProduct);
+}
+
+// Declaring a function named displayProducts
+function displayProducts(featured) {
+    let products_html = "";
+    let productsToDisplay = [];
+    if (featured == true) {
+        productsToDisplay = getFeaturedProducts();  
+    }
+    else {
+        productsToDisplay = allProducts;
+    }
+    for (let i of productsToDisplay) {
+        products_html += i.displayProductListing();
+    }
+    return products_html;
+}
+
+// Declaring a variable named featuredProducts to get HTML element
+let featuredProducts = $("#features_div");
+if (featuredProducts) {
+    featuredProducts.html(displayProducts(true));
+}
+
+// Declaring a variable named products to get  an HTML element
+let products = $("#products_div");
+if (products) {
+    products.html(displayProducts(false));
+}
+
+// Declare an object Form
+function Form(firstName, lastName, phoneNumber, email, comments, consent) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    this.comments = comments;
+    this.consent = consent;
+}
+
+// Creating a reference to contact us form
+const contactForm = $("#contact-form") [0];
+
+// Creating an event on submit
+if (contactForm) {
+    contactForm.addEventListener("submit", function(e) {
+      // Preventing the form from redirecting to other page
+      e.preventDefault();
+
+      const firstName = $("#first-name");
+      const lastName = $("#last-name");
+      const phoneNumber = $("#phone-number");
+      const email = $("#email");
+      const comments = $("#comments");
+      const consent = $("#consent");
+
+      // forms array to hold Form objects
+      var forms = new Array();
+
+      // creating a new instance of the form type
+      let newForm = new Form(firstName.value, lastName.value, phoneNumber.value, email.value, comments.value, consent.value);
+
+      // Adding a new form to the forms
+      forms.push(newForm);
+
+      // clearing the form entries
+      $("#contact-form").trigger("reset");
+
+      // displaying a message for the successful form submission
+      $(".modal-message > p").text("Thank you! We will get back to you soon! ");
+      $("#modalStyle").modal('show');
+
+      });
+}
